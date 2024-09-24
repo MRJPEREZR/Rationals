644d5a617c7f1ac035acd69f0403ef2b9d74bd7b
Julian Perez Ramirez <jperez.julian13@gmail.com>
2024-09-24 22:57:33 +0200

    updating files

--- com.tp.rational/src/Rational.java
+++ com.tp.rational/src/Rational.java
@@ -1,3 +1,5 @@
+package com.tp.rational;
+
 public class Rational {
 	
 	private int numerator;
--- com.tp.rational/src/UndefinedRational.java
+++ com.tp.rational/src/UndefinedRational.java
@@ -1,6 +1,6 @@
+package com.tp.rational;
+
 public class UndefinedRational extends Exception {
-	
-	private static final long serialVersionUID = 1L;
 
 	public UndefinedRational(String errorMessage) {
         super(errorMessage);
